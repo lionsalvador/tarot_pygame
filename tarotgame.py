177e3fc2d7a46105398458def704113445b3b648
lion <lion.salvador@proton.me>
2025-01-05 00:02:10 +0100

    fresh start

--- tarotgame.py
+++ tarotgame.py
@@ -2,21 +2,26 @@ import pygame
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 700
+SCREEN_HEIGHT = 500
+icon = pygame.image.load("res/pygame_icon.png")
 
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.display.set_caption("Tarot Game")
+pygame.display.set_icon(icon)
 
 # Game loop
 run = True
 while run:
 
     screen.fill((128, 0 ,32))
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
     
+    #event handler windows quit clicked
+    for event in pygame.event.get():
+        #quit game
+        if event.type == pygame.QUIT:
+            run =False
+
     pygame.display.update()
 
-pygame.quit
+pygame.quit()
